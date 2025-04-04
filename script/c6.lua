33b02c023ffbb8ae7371c29653e527e740df349e
Soul-8691 <peterswanton191@bellsouth.net>
2025-04-04 13:08:46 -0400

    Giant Soldier of Titanium - hopeful effect fix

--- script/c6.lua
+++ script/c6.lua
@@ -1,44 +1,45 @@
 local s,id=GetID()
 function s.initial_effect(c)
-    --(1) During the Tribute Summon cost, check if a monster with >=1800 DEF was used
-    local e0=Effect.CreateEffect(c)
-    e0:SetType(EFFECT_TYPE_SINGLE)
-    e0:SetCode(EFFECT_SUMMON_COST)
-    e0:SetOperation(s.regop)
-    c:RegisterEffect(e0)
-    
-    --(2) On successful Tribute Summon, if flagged, increase DEF by 500
+    -- Summon Success: if Tribute Summoned and label set by material check equals 1, gain 500 DEF
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
     e1:SetCode(EVENT_SUMMON_SUCCESS)
     e1:SetCondition(s.defcon)
     e1:SetOperation(s.defop)
     c:RegisterEffect(e1)
+    -- Material Check: set a label on e1 if any tribute material has 1800 or more DEF
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_MATERIAL_CHECK)
+    e2:SetValue(s.valcheck)
+    e2:SetLabelObject(e1)
+    c:RegisterEffect(e2)
 end
 
--- Store a flag if any tributed monster had 1800+ DEF
-function s.regop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    local g=Duel.GetTributeGroup(c)            -- The group of tributed monsters
-    if g and g:IsExists(Card.IsDefenseAbove,1,nil,1800) then
-        -- If at least one tributed monster has >=1800 DEF, set a flag
-        c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD_DISABLE,0,1)
+-- Check tribute materials: if any have 1800+ DEF, set label to 1; otherwise, 0.
+function s.valcheck(e,c)
+    local g=c:GetMaterial()
+    local flag=0
+    if g:IsExists(function(tc) return tc:GetDefense()>=1800 end,1,nil) then
+        flag=1
     end
+    e:GetLabelObject():SetLabel(flag)
 end
 
--- Check that this card was actually Tribute Summoned and the flag is present
+-- Condition: This monster must be Tribute Summoned and the label from the material check equals 1.
 function s.defcon(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    return c:IsSummonType(SUMMON_TYPE_ADVANCE) and c:GetFlagEffect(id)>0
+    return e:GetHandler():IsSummonType(SUMMON_TYPE_ADVANCE) and e:GetLabel()==1
 end
 
--- Increase this card’s DEF by 500
+-- Operation: Increase this monster's DEF by 500.
 function s.defop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
-    local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetCode(EFFECT_UPDATE_DEFENSE)
-    e1:SetValue(500)
-    e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
-    c:RegisterEffect(e1)
+    if c:IsFaceup() and c:IsRelateToEffect(e) then
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_DEFENSE)
+        e1:SetValue(500)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+        c:RegisterEffect(e1)
+    end
 end
