6223719af2294dc95216ddf33bdc1ccaa5e7473c
Soul-8691 <peterswanton191@bellsouth.net>
2025-04-04 13:29:44 -0400

    Titanium hopeful fix

--- script/c6.lua
+++ script/c6.lua
@@ -1,46 +1,53 @@
 local s,id=GetID()
 function s.initial_effect(c)
-    -- (1) Trigger on Summon Success (Tribute Summon)
+    -- (1) Tribute Summon Success
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
     e1:SetCode(EVENT_SUMMON_SUCCESS)
     e1:SetCondition(s.defcon)
     e1:SetOperation(s.defop)
     c:RegisterEffect(e1)
-    -- (2) Trigger on Set Success (Tribute Set)
+
+    -- (2) Tribute Set Success
     local e1b=Effect.CreateEffect(c)
     e1b:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
     e1b:SetCode(EVENT_MSET)
     e1b:SetCondition(s.defcon)
     e1b:SetOperation(s.defop)
     c:RegisterEffect(e1b)
-    -- (3) Material Check: store a flag if any tribute material has 1800 or more DEF
+
+    -- (3) Material Check
     local e2=Effect.CreateEffect(c)
     e2:SetType(EFFECT_TYPE_SINGLE)
     e2:SetCode(EFFECT_MATERIAL_CHECK)
+    e2:SetLabelObject(e1) -- associate with first effect
     e2:SetValue(s.valcheck)
     c:RegisterEffect(e2)
+
+    -- clone for set effect
+    local e2b=e2:Clone()
+    e2b:SetLabelObject(e1b)
+    c:RegisterEffect(e2b)
 end
 
--- Material Check: if any tribute material has 1800 or more DEF, store flag=1 on the card.
+-- Check for tribute material with 1800+ DEF
 function s.valcheck(e,c)
     local g=c:GetMaterial()
-    local flag = 0
+    local label=0
     if g:IsExists(function(tc) return tc:GetDefense()>=1800 end,1,nil) then
-        flag = 1
+        label=1
     end
-    c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1,flag)
+    e:GetLabelObject():SetLabel(label)
 end
 
--- Condition: If the card was either Tribute Summoned (face-up) or Tribute Set (face-down)
--- and the stored flag equals 1, then the condition is met.
+-- Only continue if summon type is valid and label == 1
 function s.defcon(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     return (c:IsSummonType(SUMMON_TYPE_ADVANCE) or c:IsSummonType(SUMMON_TYPE_MSET))
-       and c:GetFlagEffectLabel(id)==1
+       and e:GetLabel()==1
 end
 
--- Operation: Increase this monster's DEF by 500.
+-- Gain 500 DEF
 function s.defop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     if c:IsRelateToEffect(e) and c:IsFaceup() then
