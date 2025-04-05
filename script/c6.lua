0e0cfd825c3af22623fbda37f20c2732fa72c734
Soul-8691 <peterswanton191@bellsouth.net>
2025-04-05 09:55:19 -0400

    Titanium normal -> flip effect

--- script/c6.lua
+++ script/c6.lua
@@ -3,7 +3,7 @@ function s.initial_effect(c)
     -- (1) Trigger on Tribute Summon
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-    e1:SetCode(EVENT_SUMMON_SUCCESS)
+    e1:SetCode(EVENT_FLIP)
     e1:SetCondition(s.defcon)
     e1:SetOperation(s.defop)
     c:RegisterEffect(e1)
