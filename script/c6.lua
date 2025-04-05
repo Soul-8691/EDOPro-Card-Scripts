1e76d8704fab8ee8526f452de924878bf6c3d831
Soul-8691 <peterswanton191@bellsouth.net>
2025-04-05 11:38:11 -0400

    Update Titanium Lua script comment / Add 2 custom Fusion monsters

--- script/c6.lua
+++ script/c6.lua
@@ -1,6 +1,6 @@
 local s,id=GetID()
 function s.initial_effect(c)
-    -- (1) Trigger on Tribute Summon
+    -- (1) Trigger on Flip Summon
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
     e1:SetCode(EVENT_FLIP)
