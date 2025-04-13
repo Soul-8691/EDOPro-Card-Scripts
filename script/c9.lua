bc341881d23b4d3324534d33c665cbdac62f688c
Soul-8691 <peterswanton191@bellsouth.net>
2025-04-13 12:24:55 -0400

    Update Wicked Tunnel

--- script/c9.lua
+++ script/c9.lua
@@ -6,29 +6,26 @@ function s.initial_effect(c)
     e0:SetCode(EVENT_FREE_CHAIN)
     c:RegisterEffect(e0)
 
-    -- (1) Boost ATK of Wicked Worm Beast
+    -- (1) Boost "The Wicked Worm Beast" by 500 ATK
     local e1=Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_FIELD)
     e1:SetCode(EFFECT_UPDATE_ATTACK)
     e1:SetRange(LOCATION_FZONE)
     e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-    e1:SetTarget(function(e,c) return c:IsCode(6205579) end)
+    e1:SetTarget(s.atktg)
     e1:SetValue(500)
     c:RegisterEffect(e1)
 
-    -- (2) Allow tribute the turn it's summoned
+    -- (2) Allow tribute the turn it is Summoned
     local e2=Effect.CreateEffect(c)
     e2:SetType(EFFECT_TYPE_FIELD)
-    e2:SetCode(EFFECT_SPIRIT_MAYNOT_RETURN)
-    e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+    e2:SetCode(EFFECT_EXTRA_RELEASE_SUM)
     e2:SetRange(LOCATION_FZONE)
     e2:SetTargetRange(LOCATION_MZONE,0)
-    e2:SetTarget(s.allow_tribute)
-    e2:SetValue(aux.FALSE) -- prevent the "may not be tributed" restriction
+    e2:SetTarget(s.atktg)
     c:RegisterEffect(e2)
 end
 
--- Allow tributing Wicked Worm Beast this turn
-function s.allow_tribute(e,c)
+function s.atktg(e,c)
     return c:IsCode(6205579)
 end
