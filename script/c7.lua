860c4c8c067a7491504ae8b524d66e1ca216f16b
Soul-8691 <peterswanton191@bellsouth.net>
2025-04-13 13:56:57 -0400

    Field Spell -> Normal Spell

--- script/c7.lua
+++ script/c7.lua
@@ -1,67 +1,45 @@
 local s,id=GetID()
 function s.initial_effect(c)
-    -- (1) Activate directly from hand
-    local e0=Effect.CreateEffect(c)
-    e0:SetType(EFFECT_TYPE_ACTIVATE)
-    e0:SetCode(EVENT_FREE_CHAIN)
-    c:RegisterEffect(e0)
-
-    -- (2) Discard 1 card from your field to Special Summon a Level 4 or lower monster from hand this turn
+    -- Activate: Discard 1 card on your side of the field; Special Summon 1 Level 4 or lower monster from hand
     local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e1:SetType(EFFECT_TYPE_IGNITION)
-    e1:SetRange(LOCATION_SZONE)
-    e1:SetCountLimit(1,id)
-    e1:SetCost(s.sacrificecost)
-    e1:SetTarget(s.sactg)
-    e1:SetOperation(s.saop)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetCost(s.cost)
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.activate)
     c:RegisterEffect(e1)
 end
 
--- Cost: Discard 1 card from your field (can be face-up or face-down)
-function s.sacrificecost(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then 
-        return Duel.IsExistingMatchingCard(Card.IsOnField,tp,LOCATION_ONFIELD,0,1,nil) 
+-- Cost: Send 1 card you control (face-up or face-down) to the GY
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then
+        return Duel.IsExistingMatchingCard(nil,tp,LOCATION_ONFIELD,0,1,nil)
     end
-    -- Select a card on the field to discard
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
-    local g=Duel.SelectMatchingCard(tp,Card.IsOnField,tp,LOCATION_ONFIELD,0,1,1,nil)
-    
-    -- Debug log to check selected card
-    Duel.Hint(HINT_MSG_LOG,tp,"Discarding card: " .. g:GetFirst():GetName())
-    
-    -- Send the selected card to the Graveyard
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+    local g=Duel.SelectMatchingCard(tp,nil,tp,LOCATION_ONFIELD,0,1,1,nil)
     Duel.SendtoGrave(g,REASON_COST)
 end
 
--- Target: Special Summon a Level 4 or lower monster from hand
-function s.sactg(e,tp,eg,ep,ev,re,r,rp,chk)
+-- Target a Level 4 or lower monster in your hand to Special Summon
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then
-        -- Check if there is a Level 4 or lower monster in the hand
-        return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp)
+        return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+            and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp)
     end
-    -- Show info about the Special Summon
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
 
--- Filter: Level 4 or lower monster
+-- Filter for Level 4 or lower monster
 function s.spfilter(c,e,tp)
     return c:IsLevelBelow(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 
--- Operation: Special Summon the monster
-function s.saop(e,tp,eg,ep,ev,re,r,rp)
-    -- Debug: Log the operation being triggered
-    Duel.Hint(HINT_MSG_LOG,tp,"Special Summon operation triggered")
-
-    -- Select a Level 4 or lower monster from hand
+-- Special Summon from hand
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
     local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
-
-    -- Debug: Check if the card selection worked
     if #g>0 then
-        Duel.Hint(HINT_MSG_LOG,tp,"Special Summoning: " .. g:GetFirst():GetName())
         Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
     end
 end
