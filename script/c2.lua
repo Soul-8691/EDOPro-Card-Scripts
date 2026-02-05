51c0c21ce023c08de9bcda65ea50f832181e7daa
Soul-8691 <peterswanton191@bellsouth.net>
2026-02-04 20:47:01 -0500

    Start on Underground Soil

---
+++ script/c2.lua
@@ -0,0 +1,47 @@
+--Underground Soil
+--Field Spell
+local s,id=GetID()
+
+-- "The Wicked Worm Beast"
+local WORM_BEAST_CODE=62
+
+function s.initial_effect(c)
+	--Activate
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e0)
+
+	--All "The Wicked Worm Beast" gain 400 ATK
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetRange(LOCATION_FZONE)
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1:SetTarget(s.atktg)
+	e1:SetValue(400)
+	c:RegisterEffect(e1)
+
+	--"The Wicked Worm Beast" can attack all opponent's monsters
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_ATTACK_ALL)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetTarget(s.atktg)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+
+	--"The Wicked Worm Beast" cannot attack directly
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e3:SetTarget(s.atktg)
+	c:RegisterEffect(e3)
+end
+
+function s.atktg(e,c)
+	return c:IsCode(WORM_BEAST_CODE)
+end
